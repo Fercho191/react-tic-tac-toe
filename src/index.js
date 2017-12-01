b9d7719970157697d574265ecd290137b256efa0
Cristian Rojas <work@cristianrojas.com>
2017-12-01 12:33:09 -0500

    Installing and configuren initial state on redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
 import './index.css';
 import Game from './components/game/game';
+import {game} from "./reducer";
+
+let store = createStore(game);
+
+console.log(store.getState());
+store.dispatch({ type: 'MARK_POSITION'});
+console.log(store.getState());
 
 ReactDOM.render(
-    <Game />,
+    <Provider store={store}>
+        <Game />
+    </Provider>,
     document.getElementById('root')
 );
---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+export const initialState = {
+    current: 0
+};
+
+export const game = (state = initialState, action) => {
+    switch (action.type){
+        case 'MARK_POSITION':return {
+            ...state,
+            current: state.current + 1
+        };
+        default:
+            return state;
+    }
+};
